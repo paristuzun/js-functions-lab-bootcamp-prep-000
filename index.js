a72b8dc1b0de802b3bc9f6cc4d267364967afaee
Paris Tuzun <paristuzun@gmail.com>
2018-07-20 02:12:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return days."toLowerCase()"
+    days."20" === days
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (days === 20) {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
   return `It's ${days} days until ${holiday}!`
 }
 
