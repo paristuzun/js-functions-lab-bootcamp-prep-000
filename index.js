7d0d7c3ef4e7d37c592d2dd94b6537fe1d3060b0
Paris Tuzun <paristuzun@gmail.com>
2018-07-20 01:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, countdown) {
-  return 
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
 }
